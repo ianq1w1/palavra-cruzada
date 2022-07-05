aee0c02405494dd96a3b8072d8c2b193c4302ae6
ian_zaaz <105610821+ianq1w1@users.noreply.github.com>
2022-07-05 00:01:28 -0300

    Update Jogo1.svelte

--- src/Jogo1.svelte
+++ src/Jogo1.svelte
@@ -3,58 +3,65 @@
 </svelte:head>
 
 <script>
-	let p1 = "ferradura" // palavra linha da cruzada 
-	let p2 = "mercadoria"
-	let p3 = "olfato"
-	let p4 = "burrada"
-	let p5 = "comprar"
-	let p6 = "voar"
-	let p7 = "ateu"
-	let p8 = "roupa"
-</script>
+	//lógica do jogo
+function Contagem(tabVazia,tabResposta) {
+	for(let i=0; i<=tabResposta.length; i++){
+		for (let j=0; j<=tabResposta[i].length; j++){
+			return tabResposta[i][j]
+			return tabResposta[i][j] 
+	}
+}
+	if(tabResposta[i][j] === tabVazia[i][j]){
+			alert('boa')
+	}else if(tabResposta[i][j] !== tabVazia[i][j]){
+		alert('opa tá errado irmão')
+}
 
-<table border="1" align="center"><!-- tabela das cruzadas-->
-	<tbody>	
-	<tr id="vazio">
-	<td id="vazio">	
-	</td>       <!--id vazio = espaçamento-->
-	<tr>
-		{#each Array.from(p5) as coisas}
-			<td>	
-				<input type="text" id="quadrado">
-			</td>
-		{/each}
-	</tr>
-	<tr>
-		{#each Array.from(p1) as coisas}<!-- loop each = quadros para escrever-->
-			<td>
-				<input type="text" id="quadrado">
-			</td>
-		{/each}
-	</tr>
-	<tr>
-		{#each Array.from(p1) as coisas}
-			<td>
-				<input type="text" id="quadrado">
-			</td>
-		{/each}
-	</tr>
-	<tr>
-		{#each Array.from(p2) as coisas}
-			<td>
-				<input type="text" id="quadrado">
-			</td>
-		{/each}
-	</tr>
-	<tr>
-		<td>
-			<input type="text" id="quadrado">
-		</td>
-	</tr>
-		{#each Array.from(p2) as coisas}
-	<tr>
-		<input type="text" id="quadrado">
-	</tr>
-		{/each}
-	</tbody>
+}
+
+	//estrutura do jogo
+let tabelaVazia = [
+	[],
+	[],
+	[],
+	[],
+	[],
+	[],
+	]
+
+let tabelaResposta = [
+	["A", "W", "Y", "Z", "K", "X","X","X","X","X"],
+	["B", "X", "X", "X", "X", "X","X","X","X","X"],
+	["C", "X", "X", "X", "X", "X","X","X","X","X"],
+	["X", "X", "X", "X", "X", "X","X","X","X","X"],
+	["X", "X", "X", "X", "X", "X","X","X","X","X"],
+	["X", "X", "X", "X", "X", "X","X","X","X","X"],
+	] 
+
+Contagem(tabelaVazia,tabelaResposta)
+
+</script>
+<!--modelo do jogo em html-->
+<h2>
+	Cruzada nível 1
+</h2>
+
+<section>
+	<center>	
+<table>
+	{#each tabelaResposta as linha,i}
+		<tr>
+			{#each tabelaResposta as dado,j}
+				<td>
+					<input type="text" maxlength="1" bind:value={tabelaVazia[i][j]}>
+				</td>
+			{/each}
+		</tr>
+	{/each}
 </table>
+	</center>
+</section>
+
+
+
+<!-- reaproveita o botão de voltar para o menu -->
